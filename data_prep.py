6c2ab2c2e264ca418c049b198656a11f6a85a917
Nicko Pickard <pickard.nicko@gmail.com>
2023-07-28 13:42:44 -0600

    commit

--- data_prep.py
+++ data_prep.py
@@ -5,13 +5,12 @@ file_path = 'Histoy/BTC-USD/BTC-USD[2015-01-01-00-00].csv'
 df = pd.read_csv(file_path)
 
 # Assuming you have a DataFrame named 'df' with a 'Close' price column
-df['5EMA'] = df['Close'].ewm(span=5, adjust=False).mean()
-df['13EMA'] = df['Close'].ewm(span=13, adjust=False).mean()
-df['50EMA'] = df['Close'].ewm(span=50, adjust=False).mean()
-df['200EMA'] = df['Close'].ewm(span=200, adjust=False).mean()
-df['800EMA'] = df['Close'].ewm(span=800, adjust=False).mean()
+df['5EMA'] = df['close'].ewm(span=5, adjust=False).mean()
+df['13EMA'] = df['close'].ewm(span=13, adjust=False).mean()
+df['50EMA'] = df['close'].ewm(span=50, adjust=False).mean()
+df['200EMA'] = df['close'].ewm(span=200, adjust=False).mean()
+df['800EMA'] = df['close'].ewm(span=800, adjust=False).mean()
 
-
-complete = df.to_csv("Processed data")
+complete = df.to_csv("Histoy/BTC-USD/Processed/Processed data")
 print(complete)
 
