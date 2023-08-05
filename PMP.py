c0a9cdc6a2d6c6c6a171ab11aeabf01607310034
CaptainPickard <pickard.nicko@gmail.com>
2023-08-04 23:52:52 -0600

    init

--- PMP.py
+++ PMP.py
@@ -8,7 +8,6 @@ import os
 # Gets Crypto Asstet
 user_input = input("Please enter at crypto ticker: ")
 ticker = f'{user_input.upper()}-USD'
-# denoted in seconds
 time = 86400 # currently looking at daily TF
 Lookback_date = '2015-01-01-00-00'
 new = HistoricalData(ticker,time,Lookback_date).retrieve_data()
@@ -29,9 +28,6 @@ pre_pross['RSI'] = ta.rsi(pre_pross.close, length=15)
 # pre_pross['Target'] = pre_pross['Adj Close']-pre_pross.Open
 # pre_pross['Target'] = pre_pross['Adj Close'].shift(-1)
 # pre_pross['TargetClass'] = [1 if pre_pross.Target[i]>0 else 0 for i in range(len(data))]
-
-# dates used for later when plotting
-
 
 # pre_pross['TargetNextClose'] = pre_pross['percentage_return'].shift(-1)
 pre_pross['TargetNextClose'] = pre_pross['Adj Close'].shift(-1)
@@ -62,9 +58,6 @@ X = np.moveaxis(X, [0], [2])
 X, yi = np.array(X), np.array(data_set_scaled[backcandles:,-1])
 y = np.reshape(yi,(len(yi), 1))
 
-# Verifying the shape of the data set to feed into the model
-# print(X.shape)
-# print(y.shape)
 
 print('\n**Splitting data into Testing and Training sets**\n')
 
@@ -75,19 +68,17 @@ y_train, y_test = y[:splitlimit], y[:splitlimit]
 
 print('\n**Fitting the Model**\n')
 
-from keras.models import Sequential
 from keras.layers import LSTM
-from keras.layers import Dropout
 from keras.layers import Dense
-from keras.layers import TimeDistributed
-from keras.layers import Input, Activation, concatenate
+from keras.layers import Input, Activation
 
 import tensorflow as tf
-import keras 
 from keras import optimizers
-from keras.callbacks import History
 from keras.models import Model
 import numpy as np
+
+X_train_normalized = tf.keras.utils.normalize(X_train, axis=1)
+X_test_normalized = tf.keras.utils.normalize(X_test, axis=1)
 
 lstm_input = Input(shape=(backcandles, 11), name='LSTM_Input')
 inputs = LSTM(150, name='first_layer')(lstm_input)
@@ -96,54 +87,67 @@ output = Activation('linear', name='output')(inputs)
 model = Model(inputs=lstm_input, outputs=output)
 adam = optimizers.Adam()
 model.compile(optimizer=adam, loss='mse')
-model.fit(x=X_train, y=y_train, batch_size=20, epochs=50, shuffle=True, validation_split = 0.1)
+model.fit(x=
+X_test_normalized, y=y_train, batch_size=15, epochs=50, shuffle=True, validation_split = 0.1)
 
 y_pred = model.predict(X_test)
 
-# Plotting 4 different timeframes of the output data
+scaling_factors = np.max(y_train, axis=0)
 
 # Inverse transform the scaled predictions 'y_pred' to the original scale
-print("Shape of y_pred:", y_pred.shape)
-print("Shape of y_test:", y_test.shape)
+y_pred_original_scale = y_pred * scaling_factors
 
-# y_pred_original_scale = scaler.inverse_transform(y_pred)
-# y_test_original_scale = scaler.inverse_transform(y_test)
+# Plotting 4 different timeframes of the output data
+# Last 500 Rows of Price
+x_close_500 = pre_pross['close'][-500:]
+x_ema_500 = pre_pross['EMA50'][-500:]
+x_ema_200 = pre_pross['EMA200'][-500:]
+x_ema_800 = pre_pross['EMA800'][-500:]
+# time_500 = pre_pross['time'][-500:]
 
-# print("Shape of orignal y_pred:", y_pred_original_scale.shape)
-# print("Shape of orignal y_test:", y_test_original_scale.shape)
-
-y_test_1000 = y_test[-1000:]
-y_pred_1000 = y_pred[-1000:]
-
+# Last 500 Rows of Dataset
 y_test_500 = y_test[-500:]
-y_pred_500 = y_pred[-500:]
+y_pred_500 = y_pred_original_scale[-500:]
 
-y_test_100 = y_test[-100:]
-y_pred_100 = y_pred[-100:]
+# Last 50 Rows of Price
+x_close_50 = pre_pross['close'][-50:]
+x_ema_50 = pre_pross['EMA50'][-50:]
+x_ema_5 = pre_pross['EMA5'][-50:]
+x_ema_13 = pre_pross['EMA13'][-50:]
+# time_50 = pre_pross['time'][-50:]
 
+# Last 50 Rows of Dataset
 y_test_50 = y_test[-50:]
-y_pred_50 = y_pred[-50:]
+y_pred_50 = y_pred_original_scale[-50:]
 
-fig, axes = plt.subplots(2, 2, figsize=(15, 8))
+fig, axes = plt.subplots(2, 2, figsize=(14, 9))
 
-axes[0, 0].plot(y_test_1000, color = 'black', label = 'Test')
-axes[0, 0].plot(y_pred_1000, color = 'green', label = 'Pred')
+# PRICE LONG
+axes[0, 0].plot(x_close_500, color = 'black', label = 'Close Price')
+axes[0, 0].plot(x_ema_500, color = 'green', label = '50 EMA')
+axes[0, 0].plot(x_ema_200, color = 'red', label = '200 EMA')
+axes[0, 0].plot(x_ema_800, color = 'blue', label = '800 EMA')
 axes[0, 0].legend()
-axes[0, 0].set_title('Last 1000 Rows of Dataset')
+axes[0, 0].set_title('Last 500 Rows of Price')
 axes[0, 0].grid(True)
 
+# PREDICTION LONG
 axes[0, 1].plot(y_test_500, color = 'black', label = 'Test')
-axes[0, 1].plot(y_pred_500, color = 'orange', label = 'Pred')
+axes[0, 1].plot(y_pred_500, color = 'red', label = 'Pred')
 axes[0, 1].legend()
 axes[0, 1].set_title('Last 500 Rows of Dataset')
 axes[0, 1].grid(True)
 
-axes[1, 0].plot(y_test_100, color = 'black', label = 'Test')
-axes[1, 0].plot(y_pred_100, color = 'blue', label = 'Pred')
+# PRICE SHORT
+axes[1, 0].plot(x_close_50, color = 'black', label = 'Close Price')
+axes[1, 0].plot(x_ema_5, color = 'green', label = '5 EMA')
+axes[1, 0].plot(x_ema_13, color = 'red', label = '13 EMA')
+axes[1, 0].plot(x_ema_50, color = 'blue', label = '50 EMA')
 axes[1, 0].legend()
-axes[1, 0].set_title('Last 100 Rows of Dataset')
+axes[1, 0].set_title('Last 50 Rows of Price')
 axes[1, 0].grid(True)
 
+# PREDICTION SHORT
 axes[1, 1].plot(y_test_50, color = 'black', label = 'Test')
 axes[1, 1].plot(y_pred_50, color = 'red', label = 'Pred')
 axes[1, 1].legend()
