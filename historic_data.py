89f04e28c02819203c38d67e92c1a88567548115
Nicko Pickard <pickard.nicko@gmail.com>
2023-08-15 11:57:42 -0600

    init

--- historic_data.py
+++ historic_data.py
@@ -23,7 +23,8 @@ def get_user_forex():
     ticker = f'{user_input.upper()}=x'
     formatted_date = today.strftime('%Y-%m-%d')
     forex_pair = ticker
-    start_date = "1900-01-01"
+    user_input_year = input("\n Please enter a starting year (1900 for all time): ")
+    start_date = f"{user_input_year}-01-01"
     end_date = formatted_date
     pre_pross = yf.download(forex_pair, start=start_date, end=end_date)
     return pre_pross, ticker
