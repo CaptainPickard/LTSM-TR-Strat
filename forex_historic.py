6fb502b84bde5710341cfb240b09f34573f2a6d7
Nicko Pickard <pickard.nicko@gmail.com>
2023-08-15 11:56:15 -0600

    init

--- forex_historic.py
+++ forex_historic.py
@@ -5,13 +5,15 @@ from datetime import datetime
 def get_user_forex():
     today = datetime.today()
     
-    print("\nEURUSD GBPUSD USDJPY AUDUSD USDCAD\n")
-    user_input = input("\nPlease enter at Forex ticker: \n")
+    print("\n EURUSD GBPUSD USDJPY AUDUSD USDCAD\n")
+    user_input = input("\n Please enter at Forex ticker: ")
 
     ticker = f'{user_input.upper()}=x'
     formatted_date = today.strftime('%Y-%m-%d')
     forex_pair = ticker
-    start_date = "1900-01-01"
+    
+    user_input_year = input("\n Please enter a starting year (1900 for all time): ")
+    start_date = f"{user_input_year}-01-01"
     end_date = formatted_date
     pre_pross = yf.download(forex_pair, start=start_date, end=end_date)
     print(pre_pross)
