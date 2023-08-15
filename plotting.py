3f4a5bf5053410b517b5a887c33b0c72452bb71e
CaptainPickard <pickard.nicko@gmail.com>
2023-08-14 20:07:23 -0600

    added linear regression

--- plotting.py
+++ plotting.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
 
 def plotting_crypt(pre_pross, y_test, y_pred_original_scale, df_reset):
     print('\n**Defining and plotting the data**\n')
@@ -30,11 +33,8 @@ def plotting_crypt(pre_pross, y_test, y_pred_original_scale, df_reset):
     y_pred_50 = y_pred_original_scale[-50:]
     # x_data_500.set_index('date', inplace=True)
     
-    
-    
     fig, axes = plt.subplots(2, 2, figsize=(10, 5))
 
-    
     axes[0, 0].plot(x_close_500, color = 'black', label = 'Close Price')
     axes[0, 0].plot(x_ema_500, color = 'green', label = '50 EMA')
     axes[0, 0].plot(x_ema_200, color = 'red', label = '200 EMA')
@@ -73,6 +73,25 @@ def plotting_crypt(pre_pross, y_test, y_pred_original_scale, df_reset):
 def plotting_forex(pre_pross, y_test, y_pred_original_scale, df_reset):
     print('\n**Defining and plotting the data**\n')
 
+    time_short = df_reset['date'][-500:]
+    price_short = df_reset['close'][-500:]
+    
+    time_long = df_reset['date'][-50:]
+    price_long = df_reset['close'][-50:]
+    
+    model_short = LinearRegression()
+    model_long = LinearRegression()
+    
+    time_short = np.array(time_short).reshape(-1, 1)
+    time_long = np.array(time_long).reshape(-1, 1)
+    
+    # Fit the models
+    model_short.fit(time_short, price_short)
+    model_long.fit(time_long, price_long)
+
+    # Predictions for short-term and long-term trends
+    price_pred_short = model_short.predict(time_short)
+    price_pred_long = model_long.predict(time_long)
     # Plotting 4 different timeframes of the output data
     # Last 500 Rows of Price
     # x_data_500 = df_reset[-500:]
@@ -127,10 +146,13 @@ def plotting_forex(pre_pross, y_test, y_pred_original_scale, df_reset):
 
     # PREDICTION SHORT
     axes[1, 1].plot(y_test_50, color = 'black', label = 'Test')
-    axes[1, 1].plot(y_pred_50, color = 'red', label = 'Pred')
-    axes[1, 1].legend()
-    axes[1, 1].set_title('Last 50 Rows of Dataset')
-    axes[1, 1].grid(True)
+    axes[1, 1].set_title("Forex Trading Pair Price Trends")
+    axes[1, 1].xlabel("Time")
+    axes[1, 1].ylabel("Price")
+    axes[1, 1].scatter(time_short, price_short, color='blue', label='Short-term Data')
+    axes[1, 1].scatter(time_long, price_long, color='orange', label='Long-term Data')
+    axes[1, 1].plot(time_short, price_pred_short, color='blue', linestyle='dashed', label='Short-term Trend')
+    axes[1, 1].plot(time_long, price_pred_long, color='orange', linestyle='dashed', label='Long-term Trend')
 
     plt.tight_layout()
     plt.show()
